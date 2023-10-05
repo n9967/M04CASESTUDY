ce699d780059dfd7ec5423157aec5e7374f070f0
n9967 <143535191+n9967@users.noreply.github.com>
2023-10-05 16:29:49 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///data.db'
+
+
+class Drink(db.nodel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+    return{"drinks": output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return ({"name": drink.name, "description": drink.description})
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink():
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found"}
+    db.session.delete(drink)
+    db,session.commit()
+    return{"message": "yeet!"}
